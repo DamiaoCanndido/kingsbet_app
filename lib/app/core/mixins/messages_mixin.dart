0e2ef8340905260aa75cb2f2d05e63b18910e624
DamiaoCanndido <tower_gamer@gmail.com>
2022-09-10 15:23:11 -0300

    loader and message mixin

---
+++ lib/app/core/mixins/messages_mixin.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+mixin MessagesMixin on GetxController {
+  void messageListener(Rxn<MessageModel> message) {
+    ever<MessageModel?>(message, (model) async {
+      if (model != null) {
+        Get.snackbar(
+          model.title,
+          model.message,
+          backgroundColor: model.type.snackColor(),
+          colorText: model.type.textColor(),
+          margin: const EdgeInsets.all(20),
+          duration: const Duration(seconds: 3),
+          mainButton: TextButton(
+            onPressed: () {
+              Get.back();
+            },
+            child: const Text(
+              'Fechar',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        );
+      }
+    });
+  }
+}
+
+class MessageModel {
+  final String title;
+  final String message;
+  final MessageType type;
+
+  MessageModel({
+    required this.title,
+    required this.message,
+    required this.type,
+  });
+}
+
+enum MessageType {
+  error(Color.fromARGB(0, 0, 0, 0)),
+  info(Color.fromARGB(0, 0, 0, 0));
+
+  final Color color;
+  const MessageType(this.color);
+
+  Color snackColor() {
+    switch (this) {
+      case MessageType.error:
+        return Colors.red[800]!;
+      case MessageType.info:
+        return Colors.blue[200]!;
+    }
+  }
+
+  Color textColor() {
+    switch (this) {
+      case MessageType.error:
+        return Colors.white;
+      case MessageType.info:
+        return Colors.black;
+    }
+  }
+}
--- lib/app/modules/signup/signup_controller.dart
+++ lib/app/modules/signup/signup_controller.dart
@@ -1,9 +1,22 @@
 import 'package:get/get.dart';
+import 'package:kingsbet_app/app/core/mixins/loader_mixin.dart';
+import 'package:kingsbet_app/app/core/mixins/messages_mixin.dart';
 import 'package:kingsbet_app/app/repositories/auth/auth_repository.dart';
 
-class SignupController extends GetxController {
+class SignupController extends GetxController with LoaderMixin, MessagesMixin {
+  // ignore: unused_field
   final AuthRepository _authRepository;
+
+  final loading = false.obs;
+  final message = Rxn<MessageModel>();
 
   SignupController({required AuthRepository authRepository})
       : _authRepository = authRepository;
+
+  @override
+  void onInit() {
+    loaderListener(loading);
+    messageListener(message);
+    super.onInit();
+  }
 }
