6415c701bd4d99efd943034b59911d692ddc1848
DamiaoCanndido <tower_gamer@gmail.com>
2022-09-26 21:41:04 -0300

    Customize phase widget 1

--- lib/app/modules/phase/widgets/phase_tile.dart
+++ lib/app/modules/phase/widgets/phase_tile.dart
@@ -7,6 +7,50 @@ class PhaseTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(phaseModel.order.toString());
+    // TODO: ExpansionPanelList
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+          ),
+          padding: const EdgeInsets.all(10),
+          height: 50,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(6),
+            ),
+            color: Colors.yellow,
+          ),
+          child: Text(
+            "RODADA ${phaseModel.order.toString()}",
+            textAlign: TextAlign.left,
+          ),
+        ),
+        Container(
+          height: 320,
+          margin: const EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+          ),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(6),
+            ),
+            color: Colors.green,
+          ),
+          child: ListView.builder(
+            itemBuilder: (ctx, idx) {
+              return const Text("W");
+            },
+            itemCount: 10,
+          ),
+        )
+      ],
+    );
   }
 }
