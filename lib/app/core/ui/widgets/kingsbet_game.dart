13abe91af54149ca6216188fb4f2e3aac4e4fb44
DamiaoCanndido <tower_gamer@gmail.com>
2022-09-27 19:55:08 -0300

    Game component 2

--- lib/app/core/ui/widgets/kingsbet_game.dart
+++ lib/app/core/ui/widgets/kingsbet_game.dart
@@ -14,6 +14,69 @@ class KingsbetGame extends StatelessWidget {
         ),
         color: Colors.white,
       ),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: const Center(
+                child: Text(
+                  "Lusail 24-11-2022 thu 16:00",
+                  style: TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.red,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.orange,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.yellow,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: const Center(
+                child: Text(
+                  "Copa do mundo, Grupo G, rodada 1",
+                  style: TextStyle(
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
